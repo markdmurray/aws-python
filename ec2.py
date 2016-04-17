c2f5c807ee53b7cf57fa52fa3b9f59260981c5ea
mark murray <m@rkmurray.co>
2016-04-17 21:18:22 +1000

    working security group creation, working return of sgid

--- ec2.py
+++ ec2.py
@@ -1,13 +1,24 @@
+# key pair creation
+
 import boto3
 
-
-def main(keyname):
+ec2 = boto3.client('ec2')
+def create_key(keyname):
+    #create file in users ssh directory to write contents of key
     f = open("/home/mark/.ssh/%s" % keyname, "w")
-    ec2 = boto3.client('ec2')
     resp = ec2.create_key_pair(
         KeyName=keyname
         )
     key_contents = resp['KeyMaterial']
+    # write contents of key to file
     f.write(key_contents)
 
-main("testkey2")
+def create_security_group(groupname):
+    resp = ec2.create_security_group(
+            GroupName=groupname,
+            Description="Automated dev instance",
+            VpcId='vpc-a92e55cc'
+            )
+    groupid = resp['GroupId']
+    print(groupid)
+create_security_group('auto')
