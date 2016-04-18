6e08de45051288a020a4d7200ca2491dfaf4967b
mark murray <m@rkmurray.co>
2016-04-18 10:16:27 +1000

    missing protocol parameter

--- ec2.py
+++ ec2.py
@@ -1,6 +1,14 @@
 # key pair creation
 
 import boto3
+import socket
+import dns.resolver
+
+def get_ip():
+    resolver = dns.resolver.Resolver()
+    resolver.nameservers = resolver.nameservers=[socket.gethostbyname('resolver1.opendns.com')]
+    for rdata in resolver.query('myip.opendns.com','A'):
+        return rdata
 
 ec2 = boto3.client('ec2')
 def create_key(keyname):
@@ -13,12 +21,24 @@ def create_key(keyname):
     # write contents of key to file
     f.write(key_contents)
 
-def create_security_group(groupname):
+def create_security_group(groupname,ipaddress):
     resp = ec2.create_security_group(
             GroupName=groupname,
             Description="Automated dev instance",
             VpcId='vpc-a92e55cc'
             )
     groupid = resp['GroupId']
-    print(groupid)
-create_security_group('auto')
+    create_ingress_rules(groupid,ipaddress)
+
+def create_ingress_rules(groupid,ipaddress):
+    get_ip()
+    resp = ec2.authorize_security_group_ingress(
+            GroupId=groupid,
+
+            FromPort=22,
+            ToPort=22,
+            CidrIp='%s/32' % ipaddress
+            )
+
+ipaddress = get_ip()
+create_security_group('test',ipaddress)
