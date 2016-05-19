1758855a7018d97af4e97412d96c474f2f955970
mark murray <m@rkmurray.co>
2016-05-19 20:01:51 +1000

    created script to add current ip address to gitlab elb

---
+++ add_address_elb_gitlab.py
@@ -0,0 +1,24 @@
+import dns.resolver
+import socket
+import boto3
+
+ec2 = boto3.client('ec2')
+
+def get_ip():
+    resolver = dns.resolver.Resolver()
+    resolver.nameservers=[socket.gethostbyname('resolver1.opendns.com')]
+    for rdata in resolver.query('myip.opendns.com','A'):
+        return(rdata)
+
+def create_ingress_rules(ipaddress):
+    print(ipaddress)
+    resp = ec2.authorize_security_group_ingress(
+            GroupId='sg-5819dd3c',
+            IpProtocol='tcp',
+            FromPort=443,
+            ToPort=443,
+            CidrIp='%s/32' % ipaddress
+            )
+
+ip_address = get_ip()
+create_ingress_rules(ip_address)
